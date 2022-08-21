b17dfc66dfa4685f1b4a9b413d41c76e35b7d03d
yazui <yazui.zag@gmail.com>
2022-08-22 00:45:14 +0200

    index.vue updated

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
-    <meta charset="utf-8">
-    <link rel="shortcut icon" type="image/png" href="/static/favicon.png"/>
-    <title>Corona Cases</title>
-	<navbar/>
-	<hero/>
-	<main_footer/>
+	<meta charset="utf-8" />
+	<link rel="shortcut icon" type="image/png" href="/static/favicon.png" />
+	<title>tomegb</title>
+	<navbar />
+	<hero />
+	<main_footer />
 </template>
