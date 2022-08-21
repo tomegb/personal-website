64d82755ff2d7b92be32822c942c08757eb52a12
yazui <yazui.zag@gmail.com>
2022-08-22 00:44:48 +0200

    hero.vue updated

--- components/hero.vue
+++ components/hero.vue
@@ -1,14 +1,19 @@
 <template>
-	<div class="hero min-h-screen bg-base-100">
-		<div class="hero-content flex-col lg:flex-row">
-			<img
-				src="https://cdn.discordapp.com/attachments/932265928522088450/1003312829174136982/unknown.png"
-				class="max-w-sm rounded-lg shadow-2xl"
-			/>
-			<div>
-				<h1 class="text-5xl font-bold">apis Discord Bot</h1>
-				<p class="py-6">Your Discord Bot with your own rules.</p>
-				<button class="btn btn-primary">Invite</button>
+	<div
+		class="hero min-h-screen"
+		style="
+			background-image: url(https://cdn.discordapp.com/attachments/932265928522088450/1011043091916001420/3554x1999_iceland-hofn-mountains-4k.jpg);
+		"
+	>
+		<div class="hero-overlay bg-opacity-60"></div>
+		<div class="hero-content text-center text-neutral-content">
+			<div class="max-w-md">
+				<h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+				<p class="mb-5">
+					Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+					exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+				</p>
+				<button class="btn btn-primary">Get Started</button>
 			</div>
 		</div>
 	</div>
