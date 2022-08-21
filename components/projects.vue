87945c8f1c5574aba38e7e954d919b45f60fe9da
yazui <yazui.zag@gmail.com>
2022-08-22 01:11:43 +0200

    projects.vue added

---
+++ components/projects.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="hero min-h-screen bg-neutral-focus">
+		<div class="hero-content text-center text-neutral-content"></div>
+
+		<h1 class="mb-5 text-3xl font-bold">Projects</h1>
+
+		<div class="... grid grid-cols-3 place-items-start gap-4">
+			<div class="card w-96 bg-base-100 shadow-xl">
+				<div class="card-body">
+					<h2 class="card-title">Card title!</h2>
+					<p>If a dog chews shoes whose shoes does he choose?</p>
+					<div class="card-actions justify-end">
+						<button class="btn btn-primary">Buy Now</button>
+					</div>
+				</div>
+			</div>
+			<div class="card w-96 bg-base-100 shadow-xl">
+				<div class="card-body">
+					<h2 class="card-title">Card title!</h2>
+					<p>If a dog chews shoes whose shoes does he choose?</p>
+					<div class="card-actions justify-end">
+						<button class="btn btn-primary">Buy Now</button>
+					</div>
+				</div>
+			</div>
+			<div class="card w-96 bg-base-100 shadow-xl">
+				<div class="card-body">
+					<h2 class="card-title">Card title!</h2>
+					<p>If a dog chews shoes whose shoes does he choose?</p>
+					<div class="card-actions justify-end">
+						<button class="btn btn-primary">Buy Now</button>
+					</div>
+				</div>
+			</div>
+			<div class="card w-96 bg-base-100 shadow-xl">
+				<div class="card-body">
+					<h2 class="card-title">Card title!</h2>
+					<p>If a dog chews shoes whose shoes does he choose?</p>
+					<div class="card-actions justify-end">
+						<button class="btn btn-primary">Buy Now</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
