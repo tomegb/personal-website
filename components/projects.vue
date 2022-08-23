c68607ad744efb4c0b21af7988661025926b2d52
yazui <yazui.zag@gmail.com>
2022-08-23 19:57:17 +0200

    projects.vue updated

--- components/projects.vue
+++ components/projects.vue
@@ -5,52 +5,49 @@
 				<h1 class="mb-105 text-5xl font-bold">Projects</h1>
 			</div>
 		</div>
-		<div class="m-10 mx-auto lg:flex">
-			<div class="m-6 flex w-full flex-col">
-				<div class="card w-96 bg-neutral shadow-xl">
-					<div class="card-body">
-						<h2 class="card-title">covid-cases-website</h2>
-						<p>
-							Website that evaluates the current corona cases and makes them available to the
-							public..
-						</p>
-						<div class="card-actions justify-end">
-							<font-awesome-icon icon="fab fa-github" />
-							<div class="badge badge-outline badge-accent">vue</div>
-						</div>
+		<div class="... flex justify-around">
+			<div class="card w-96 bg-neutral shadow-xl">
+				<div class="card-body">
+					<h2 class="card-title">covid-cases-website</h2>
+					<p>
+						Website that evaluates the current corona cases and makes them available to the public..
+					</p>
+					<div class="card-actions justify-end">
+						<font-awesome-icon icon="fab fa-github" />
+						<div class="badge badge-outline badge-accent">vue</div>
 					</div>
 				</div>
 			</div>
-			<div class="m-6 flex w-full flex-col overflow-hidden">
-				<div class="card w-96 bg-neutral shadow-xl">
-					<div class="card-body">
-						<h2 class="card-title">personal-website</h2>
-						<p>My personal website created with vue, nuxt3 and tailwind.</p>
-						<div class="card-actions justify-end">
-							<div class="badge badge-outline badge-accent">vue</div>
-						</div>
+		</div>
+		<div class="m-6 flex w-full flex-col overflow-hidden">
+			<div class="card w-96 bg-neutral shadow-xl">
+				<div class="card-body">
+					<h2 class="card-title">personal-website</h2>
+					<p>My personal website created with vue, nuxt3 and tailwind.</p>
+					<div class="card-actions justify-end">
+						<div class="badge badge-outline badge-accent">vue</div>
 					</div>
 				</div>
 			</div>
-			<div class="m-6 flex w-full flex-col overflow-hidden">
-				<div class="card w-96 bg-neutral shadow-xl">
-					<div class="card-body">
-						<h2 class="card-title">covidcord-discord-bot</h2>
-						<p>A JDA based Discord bot that can display current Covid numbers from the RKI.</p>
-						<div class="card-actions justify-end">
-							<div class="badge badge-outline badge-warning">Java</div>
-						</div>
+		</div>
+		<div class="m-6 flex w-full flex-col overflow-hidden">
+			<div class="card w-96 bg-neutral shadow-xl">
+				<div class="card-body">
+					<h2 class="card-title">covidcord-discord-bot</h2>
+					<p>A JDA based Discord bot that can display current Covid numbers from the RKI.</p>
+					<div class="card-actions justify-end">
+						<div class="badge badge-outline badge-warning">Java</div>
 					</div>
 				</div>
 			</div>
-			<div class="m-6 flex w-full flex-col overflow-hidden">
-				<div class="card w-96 bg-neutral shadow-xl">
-					<div class="card-body">
-						<h2 class="card-title">apis-website</h2>
-						<p>A website for the apis discord bot.</p>
-						<div class="card-actions justify-end">
-							<div class="badge badge-outline badge-accent">vue</div>
-						</div>
+		</div>
+		<div class="m-6 flex w-full flex-col overflow-hidden">
+			<div class="card w-96 bg-neutral shadow-xl">
+				<div class="card-body">
+					<h2 class="card-title">apis-website</h2>
+					<p>A website for the apis discord bot.</p>
+					<div class="card-actions justify-end">
+						<div class="badge badge-outline badge-accent">vue</div>
 					</div>
 				</div>
 			</div>
