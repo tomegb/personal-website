c590333b4032e4934ab305102e04c1e6679b22f9
yazui <yazui.zag@gmail.com>
2022-08-23 10:58:02 +0200

    projects.vue updated

--- components/projects.vue
+++ components/projects.vue
@@ -1,43 +1,56 @@
 <template>
-	<div class="hero min-h-screen bg-neutral-focus">
-		<div class="hero-content text-center text-neutral-content"></div>
-
-		<h1 class="mb-5 text-3xl font-bold">Projects</h1>
-
-		<div class="... grid grid-cols-3 place-items-start gap-4">
-			<div class="card w-96 bg-base-100 shadow-xl">
-				<div class="card-body">
-					<h2 class="card-title">Card title!</h2>
-					<p>If a dog chews shoes whose shoes does he choose?</p>
-					<div class="card-actions justify-end">
-						<button class="btn btn-primary">Buy Now</button>
+	<div class="min-h-screen bg-neutral-focus">
+		<div class="... p-10">
+			<div class="... flex justify-center">
+				<h1 class="mb-105 text-5xl font-bold">Projects</h1>
+			</div>
+		</div>
+		<div class="m-10 mx-auto lg:flex">
+			<div class="m-6 flex w-full flex-col">
+				<div class="card w-96 bg-neutral shadow-xl">
+					<div class="card-body">
+						<h2 class="card-title">covid-cases-website</h2>
+						<p>
+							Website that evaluates the current corona cases and makes them available to the
+							public..
+						</p>
+						<div class="card-actions justify-end">
+							<font-awesome-icon icon="fab fa-github" />
+							<div class="badge badge-outline badge-accent">vue</div>
+						</div>
 					</div>
 				</div>
 			</div>
-			<div class="card w-96 bg-base-100 shadow-xl">
-				<div class="card-body">
-					<h2 class="card-title">Card title!</h2>
-					<p>If a dog chews shoes whose shoes does he choose?</p>
-					<div class="card-actions justify-end">
-						<button class="btn btn-primary">Buy Now</button>
+			<div class="m-6 flex w-full flex-col overflow-hidden">
+				<div class="card w-96 bg-neutral shadow-xl">
+					<div class="card-body">
+						<h2 class="card-title">personal-website</h2>
+						<p>My personal website created with vue, nuxt3 and tailwind.</p>
+						<div class="card-actions justify-end">
+							<div class="badge badge-outline badge-accent">vue</div>
+						</div>
 					</div>
 				</div>
 			</div>
-			<div class="card w-96 bg-base-100 shadow-xl">
-				<div class="card-body">
-					<h2 class="card-title">Card title!</h2>
-					<p>If a dog chews shoes whose shoes does he choose?</p>
-					<div class="card-actions justify-end">
-						<button class="btn btn-primary">Buy Now</button>
+			<div class="m-6 flex w-full flex-col overflow-hidden">
+				<div class="card w-96 bg-neutral shadow-xl">
+					<div class="card-body">
+						<h2 class="card-title">covidcord-discord-bot</h2>
+						<p>A JDA based Discord bot that can display current Covid numbers from the RKI.</p>
+						<div class="card-actions justify-end">
+							<div class="badge badge-outline badge-warning">Java</div>
+						</div>
 					</div>
 				</div>
 			</div>
-			<div class="card w-96 bg-base-100 shadow-xl">
-				<div class="card-body">
-					<h2 class="card-title">Card title!</h2>
-					<p>If a dog chews shoes whose shoes does he choose?</p>
-					<div class="card-actions justify-end">
-						<button class="btn btn-primary">Buy Now</button>
+			<div class="m-6 flex w-full flex-col overflow-hidden">
+				<div class="card w-96 bg-neutral shadow-xl">
+					<div class="card-body">
+						<h2 class="card-title">apis-website</h2>
+						<p>A website for the apis discord bot.</p>
+						<div class="card-actions justify-end">
+							<div class="badge badge-outline badge-accent">vue</div>
+						</div>
 					</div>
 				</div>
 			</div>
