bd14fa06356ed79cefa3944760380313e630cb15
Tom Egbers <mail@tomegb.cloud>
2022-10-03 22:09:32 +0200

    Create content.vue

---
+++ components/content.vue
@@ -0,0 +1,86 @@
+<template>
+	<div class="min-h-screen bg-neutral-focus">
+		<div class="... p-14">
+			<div class="... flex justify-center">
+				<h1 class="mb-105 text-5xl font-bold">Projects</h1>
+			</div>
+		</div>
+		<div class="... flex justify-center">
+			<div class="flex flex-wrap">
+				<div class="... px-6">
+					<div class="... pt-6">
+						<div class="card w-96 bg-neutral shadow-xl">
+							<div class="card-body">
+								<h2 class="card-title">covid-cases-website</h2>
+								<p>Website that evaluates the current corona numbers.</p>
+								<div class="card-actions justify-end">
+									<font-awesome-icon icon="fab fa-github" />
+									<div class="badge badge-outline badge-accent">vue</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="... px-6">
+					<div class="... pt-6">
+						<div class="card w-96 bg-neutral shadow-xl">
+							<div class="card-body">
+								<h2 class="card-title">personal-website</h2>
+								<p>My personal website created with vue, nuxt3 and tailwind.</p>
+								<div class="card-actions justify-end">
+									<div class="badge badge-outline badge-accent">vue</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="... px-6">
+					<div class="... pt-6">
+						<div class="card w-96 bg-neutral shadow-xl">
+							<div class="card-body">
+								<h2 class="card-title">covidcord-discord-bot</h2>
+								<p>A JDA based Discord bot that can display current Covid numbers from the RKI.</p>
+								<div class="card-actions justify-end">
+									<div class="badge badge-outline badge-warning">Java</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="... px-6">
+					<div class="... pt-6">
+						<div class="card w-96 bg-neutral shadow-xl">
+							<div class="card-body">
+								<h2 class="card-title">apis-website</h2>
+								<p>A website for the apis discord bot with live bot numbers.</p>
+								<div class="card-actions justify-end">
+									<div class="badge badge-outline badge-accent">vue</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="... p-24">
+			<div class="... flex justify-center">
+				<h1 class="mb-105 text-5xl font-bold">Examples</h1>
+			</div>
+		</div>
+		<div class="... pb-16">
+			<div class="... flex justify-center">
+				<div class="mockup-phone">
+					<div class="camera"></div>
+					<div class="display">
+						<div class="artboard artboard-demo phone-1">
+							<img
+								src="https://cdn.discordapp.com/attachments/1013433809221455902/1013506592806805514/youtube_1.png"
+								class="max-w-sm rounded-lg"
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
