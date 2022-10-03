b1d238530f0ffbedf631ac5c8f5b18192f057176
Tom Egbers <mail@tomegb.cloud>
2022-10-03 20:37:06 +0200

    Update navbar.vue

---
+++ components/navbar.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="navbar fixed top-0 w-full bg-black bg-opacity-50">
+		<div class="flex-1">
+			<a class="btn btn-ghost text-xl normal-case">tomegb</a>
+		</div>
+		<div class="flex-none">
+			<button class="btn btn-ghost btn-square">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					class="inline-block h-5 w-5 stroke-current"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+					></path>
+				</svg>
+			</button>
+		</div>
+	</div>
+</template>
