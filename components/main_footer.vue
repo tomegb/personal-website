61e457be398998d28c87129d83bb7a18484db474
Tom Egbers <mail@tomegb.cloud>
2022-10-03 22:09:34 +0200

    Update main_footer.vue

--- components/main_footer.vue
+++ components/main_footer.vue
@@ -1,10 +1,10 @@
 <template>
 	<footer class="footer footer-center rounded bg-neutral p-10 text-base-content">
 		<div class="grid grid-flow-col gap-4">
-			<a class="link link-hover">Imprint</a>
-			<a class="link link-hover">Privacy Policy</a>
-			<a class="link link-hover">Disclaimer</a>
-			<a class="link link-hover">Status</a>
+			<nuxt-link :to="'/imprint'" class="link link-hover">Imprint</nuxt-link>
+			<nuxt-link :to="'/privacypolicy'" class="link link-hover">Privacy Policy</nuxt-link>
+			<nuxt-link :to="'/disclaimer'" class="link link-hover">Disclaimer</nuxt-link>
+			<nuxt-link :to="'https://status.tomegb.systems'" class="link link-hover">Status</nuxt-link>
 		</div>
 		<div>
 			<p>Copyright © 2022 - All right reserved by tomegb.cloud</p>
