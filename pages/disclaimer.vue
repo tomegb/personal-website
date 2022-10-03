d908776a5e9e694a110740691022701798155b54
Tom Egbers <mail@tomegb.cloud>
2022-10-03 22:09:41 +0200

    Create disclaimer.vue

---
+++ pages/disclaimer.vue
@@ -0,0 +1,8 @@
+<template>
+	<meta charset="utf-8" />
+	<link rel="shortcut icon" type="image/png" href="/static/favicon.png" />
+	<title>tomegb - disclaimer</title>
+	<navbar />
+	<div class="min-h-screen bg-neutral-focus"></div>
+	<main_footer />
+</template>
