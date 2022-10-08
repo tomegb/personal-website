81e889cefded47e0755482a66d2bfc6a04c3ff3c
Tom Egbers <mail@tomegb.cloud>
2022-10-09 01:25:34 +0200

    Update disclaimer.vue

--- pages/disclaimer.vue
+++ pages/disclaimer.vue
@@ -3,6 +3,12 @@
 	<link rel="shortcut icon" type="image/png" href="/static/favicon.png" />
 	<title>tomegb - disclaimer</title>
 	<navbar />
-	<div class="min-h-screen bg-neutral-focus"></div>
+	<div class="min-h-screen bg-neutral-focus">
+		<div class="... flex justify-center">
+			<div class="... pt-28">
+				<h1 class="mb-5 text-5xl font-bold">Disclaimer</h1>
+			</div>
+		</div>
+	</div>
 	<main_footer />
 </template>
