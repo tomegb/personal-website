3818f0d70a19c55f60c42628dd8516edaf5a9517
Tom Egbers <mail@tomegb.cloud>
2022-10-09 01:25:39 +0200

    Update privacypolicy.vue

--- pages/privacypolicy.vue
+++ pages/privacypolicy.vue
@@ -3,6 +3,12 @@
 	<link rel="shortcut icon" type="image/png" href="/static/favicon.png" />
 	<title>tomegb - privacypolicy</title>
 	<navbar />
-	<div class="min-h-screen bg-neutral-focus"></div>
+	<div class="min-h-screen bg-neutral-focus">
+		<div class="... flex justify-center">
+			<div class="... pt-28">
+				<h1 class="mb-5 text-5xl font-bold">Privacy Policy</h1>
+			</div>
+		</div>
+	</div>
 	<main_footer />
 </template>
